6e2b9060f376b91f824ba94a524e57a70db02cb2
Kota Yatagai <kota@yatagai.com>
2020-08-01 16:38:52 +0900

    commit

---
+++ Assets/CsharpScript/Door_Controller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Door_Controller : MonoBehaviour {
+    public bool isOpened = false;
+    public bool isEnter = false;
+
+    void Update () {
+        Quaternion quaternion = this.transform.rotation;
+        float currentRotation = quaternion.eulerAngles.y;
+        if (Input.GetKeyDown (KeyCode.F)) {
+            if (isEnter && !isOpened) {
+                transform.Rotate (0, 90, 0);
+                isOpened = true;
+                Debug.Log (currentRotation);
+            } else if (isEnter && isOpened) {
+                transform.Rotate (0, -90, 0);
+                isOpened = false;
+                //Debug.Log (currentRotation);
+            }
+        }
+    }
+
+    private void OnTriggerStay (Collider collider) {
+        if (collider.gameObject.tag == "Player") {
+            isEnter = true;
+        }
+    }
+    private void OnTriggerExit (Collider collider) {
+        if (collider.gameObject.tag == "Player") {
+            isEnter = false;
+        }
+    }
+}
---
+++ Assets/CsharpScript/Quit_Controller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Quit_Controller : MonoBehaviour {
+    public Button button_to_be_emulated;
+    void Update () {
+        if (Input.GetKeyUp (KeyCode.Escape)) {
+            button_to_be_emulated.onClick.Invoke ();
+        };
+        /*if (canvas.enabled) {
+            if (Input.GetKeyUp (KeyCode.Y)) OnCallExit ();
+            if (Input.GetKeyUp (KeyCode.N)) OnCallCancel ();
+        }*/
+    }
+    public void OnCallExit () {
+        UnityEngine.Application.Quit ();
+    }
+    public void StartOnClick () {
+        SceneManager.LoadScene ("Room");
+    }
+    public void BackToHomeOnClick () {
+        SceneManager.LoadScene ("Start");
+    }
+}
